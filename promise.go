a27a3ddf41f96c96e347c1dff6cc3c90f99ae3d1
Tejas Jamdade <jamdade.2@iitj.ac.in>
2023-04-18 16:58:18 +0530

    update promise
    Signed-off-by: Tejas Jamdade <jamdade.2@iitj.ac.in>

--- promise.go
+++ promise.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"reflect"
+	"sync"
 	"time"
 )
 
+// Defining the Promise Interface and the Promise Struct
+
 type Promise interface {
-	Resolve(v interface{})
-	Reject(err error)
-	Catch(f func(err error) interface{}) Promise
-	Then(f func(v interface{}) interface{}) Promise
-	Finally(f func()) Promise
-	HandlePanic()
+	Resolve(v interface{}) // Resolve Method
+	Reject(err error) // Reject Method
+	Catch(f func(err error) interface{}) Promise // Catch Method
+	Then(f func(v interface{}) interface{}) Promise // Then Method
+	Finally(f func()) Promise // Finally Method
+	HandlePanic() // HandlePanic Method
 }
 
 type promise[v any] struct {
@@ -51,8 +53,10 @@ func (p *promise[T]) Resolve(value interface{}) {
 		if val.IsValid() && val.Type().AssignableTo(reflect.TypeOf((*T)(nil)).Elem()) {
 			v := val.Interface().(T)
 			p.value = &v
+			p.state = "fulfilled"
 		} else {
 			p.err = fmt.Errorf("Resolve: cannot assign %v (type %v) to type *%v", value, val.Type(), reflect.TypeOf((*T)(nil)).Elem())
+			p.state = "rejected"
 		}
 
 		close(p.CH)
@@ -79,6 +83,9 @@ func (p *promise[T]) Catch(f func(error) interface{}) Promise {
 		<-p.CH
 		if p.err != nil {
 			result := f(p.err)
+			if (reflect.TypeOf(result) != reflect.TypeOf(*p.value)) {
+				fmt.Println("Error: Wrong return type from 'Catch' method...")
+			}
 			switch val := result.(type) {
 			case error:
 				newPromise.Reject(val)
@@ -100,7 +107,7 @@ func (p *promise[T]) Catch(f func(error) interface{}) Promise {
 	return newPromise
 }
 
-// newPromise.HandlePanic undefined (type Promise has no field or method HandlePanic)
+
 func (p *promise[T]) Then(f func(interface{}) interface{}) Promise {
 
 	newPromise := NewPromise[T]()
@@ -110,12 +117,15 @@ func (p *promise[T]) Then(f func(interface{}) interface{}) Promise {
 		if p.err != nil {
 			newPromise.Reject(p.err)
 		} else {
-			result := f(p.value)
+			result := f(*p.value) // Reading the value at the pointer
+			if (reflect.TypeOf(result) != reflect.TypeOf(*p.value)) {
+				fmt.Println("Error: Wrong return type from 'Then' method.")
+			}
 			switch val := result.(type) {
 			case error:
-				newPromise.Reject(val)
+				newPromise.Reject(val) 	// if the result is an error, reject the promise
 			default:
-				newPromise.Resolve(result)
+				newPromise.Resolve(result) // if the result is not an error, resolve the promise
 			}
 		}
 	}()
@@ -136,7 +146,11 @@ func (p *promise[T]) Finally(f func()) Promise {
 		defer newPromise.HandlePanic()
 		<-p.CH
 		f()
-		newPromise.Resolve(p.value)
+		if p.err != nil {
+			newPromise.Reject(p.err)
+		} else {
+			newPromise.Resolve(p.value)
+		}
 	}()
 
 	return newPromise
@@ -150,25 +164,72 @@ func (p *promise[T]) HandlePanic() {
 
 
 func main() {
-	// create a new promise
 
-	fmt.Println("Hello Go, we doing promises today!")
-	p := NewPromise[int]()
-
-	// chain promises with Then and Finally
-	p.Then(func(val interface{}) interface{} {
-		// multiply the value by 2
-		return val.(int) * 2
+	// First Test case
+	
+	p1 := NewPromise[int]()
+	
+	p1.Then(func(v interface{}) interface{} {
+		fmt.Println("First promise resolved with value: ", v)
+		return 10
+	}).Then(func(v interface{}) interface{} {
+		fmt.Println("Second promise resolved with value: ", v)
+		return 20
 	}).Finally(func() {
-		// print "done" when the promise is resolved or rejected
-		fmt.Println("done")
+		fmt.Printf("Promise execution finished\n\n")
 	})
 
-	// resolve the promise with a value of 5
-	p.Resolve(5)
+	p1.Resolve(5)	
 
-	// wait for the promise to be resolved or rejected
-	<-p.(*promise[int]).CH
 
+	// Second Test case 
+
+	p2 := NewPromise[int]()
+
+	p2.Then(func(v interface{}) interface{} {
+		fmt.Println("First promise resolved with value: ", v)
+		return fmt.Errorf("something went wrong") // Intentionally throwing an error to check the catch method
+	}).Catch(func(err error) interface{} {
+		fmt.Println("Error: ", err)
+		return -1
+	}).Finally(func() {
+		fmt.Printf("Promise execution finished\n\n")
+	})
+
+	p2.Resolve(10)
+
+
+	// Third Test case
+	
+	p3 := NewPromise[string]()
+
+	name, err := fetchName()
+	if err != nil {
+		p3.Reject(err)
+	} else {
+		p3.Resolve(name)
+	}
+
+
+	p3.Then(func(v interface{}) interface{} {
+		fmt.Println("My name is: ", v)
+		// return "" // Uncomment this to run wihout catch method
+		return 5 // Intentionally returning an int to check the catch method
+	}).Catch(func(err error) interface{} {
+		fmt.Println("Error: ", err)
+		return ""
+	}).Finally(func() {
+		fmt.Println("Promise execution finished")
+	})
+
+	
+	
 	time.Sleep(1 * time.Second)
+	
+ 	// wait for the promise to be resolved or rejected
+}
+
+
+func fetchName() (string, error) {
+	return "John", nil
 }
